d395601ba3a85bd5f9c306d22a0a7cfeabb610a0
Leocyberdev <151261062+Leocyberdev@users.noreply.github.com>
2025-08-04 13:58:34 -0300

    Update product.py

--- src/models/product.py
+++ src/models/product.py
@@ -1,6 +1,8 @@
 from src.database import db
 
 class Category(db.Model):
+    __tablename__ = "categories"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     products = db.relationship("Product", backref="category", lazy=True)
@@ -8,15 +10,20 @@ class Category(db.Model):
     def __repr__(self):
         return f"<Category {self.name}>"
 
+
+from src.database import db
+
 class Product(db.Model):
+    __tablename__ = "products"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
-    cost = db.Column(db.Float, nullable=True) # Novo campo para custo
+    cost = db.Column(db.Float, nullable=True)  # Novo campo para custo
     image_url = db.Column(db.String(200), nullable=True)
     is_available = db.Column(db.Boolean, default=True)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
 
     def __repr__(self):
         return f"<Product {self.name}>"
